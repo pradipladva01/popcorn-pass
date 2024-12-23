a750049bbe0a75ddf3a89d3780dd25fd470bce38
Pradip Ladva <pradipladva02@gmail.com>
2024-12-23 19:47:15 +0530

    issue solve

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <>
+    <> 
       <footer class="footer_section">
         <div class="container">
           <div class="row">
